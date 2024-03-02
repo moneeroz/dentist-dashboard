00b126be9a3166f53c5225e32d022c2b4d9223a3
Moneer <moneer.alromimah@gmail.com>
2024-03-02 15:53:27 -0700

    update

---
+++ app/ui/revenue/filter.tsx
@@ -0,0 +1,74 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useDebouncedCallback } from 'use-debounce';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { months } from '@/app/lib/months';
+
+export default function Filter() {
+  // Initialize state with URL search params or defaults
+  const searchParams = useSearchParams();
+  const initialMonth =
+    searchParams.get('month') || (new Date().getMonth() + 1).toString();
+  const initialYear =
+    searchParams.get('year') || new Date().getFullYear().toString();
+
+  // Use useState to manage month and year based on URL params or current date
+  const [month, setMonth] = useState(initialMonth);
+  const [year, setYear] = useState(initialYear);
+
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // Update the URL when month or year changes
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.set('month', month);
+    params.set('year', year);
+    replace(`${pathname}?${params.toString()}`);
+  }, [month, year, pathname, replace]);
+
+  // Handlers for changes in month and year
+  const handleYearChange = useDebouncedCallback((year) => {
+    setYear(year);
+  }, 300);
+
+  const handleMonthChange = useDebouncedCallback((month) => {
+    setMonth(month);
+  }, 300);
+
+  return (
+    <div className="mb-4 flex gap-5">
+      <div className="mb-4">
+        <label htmlFor="year" className="ml-2 text-sm font-medium">
+          العام
+        </label>
+        <input
+          id="year"
+          name="year"
+          type="text"
+          value={year}
+          onChange={(e) => handleYearChange(e.target.value)}
+          className="rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2"
+        />
+      </div>
+      <div>
+        <label htmlFor="month" className=" ml-2 text-sm font-medium">
+          الشهر
+        </label>
+        <select
+          name="month"
+          id="month"
+          value={month}
+          onChange={(e) => handleMonthChange(e.target.value)}
+          className="cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+        >
+          {months.map((m) => (
+            <option key={m.value} value={m.value}>
+              {m.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
