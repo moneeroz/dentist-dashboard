d9fe056fa4718af1ba0d822417db08ea08665f50
Moneer <moneer.alromimah@gmail.com>
2024-03-03 15:44:29 -0700

    add months util

---
+++ app/lib/months.ts
@@ -0,0 +1,14 @@
+export const months = [
+  { value: 1, label: 'يناير' },
+  { value: 2, label: 'فبراير' },
+  { value: 3, label: 'مارس' },
+  { value: 4, label: 'أبريل' },
+  { value: 5, label: 'مايو' },
+  { value: 6, label: 'يونيو' },
+  { value: 7, label: 'يوليو' },
+  { value: 8, label: 'أغسطس' },
+  { value: 9, label: 'سبتمبر' },
+  { value: 10, label: 'أكتوبر' },
+  { value: 11, label: 'نوفمبر' },
+  { value: 12, label: 'ديسمبر' },
+];
