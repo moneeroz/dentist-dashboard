9470391c58849209785e74cac35a7e9f9a265e9d
Moneer <moneer.alromimah@gmail.com>
2024-03-10 20:53:36 -0600

    add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,14 @@
+import { Lusitana, Marhey, Readex_Pro } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+export const marhey = Marhey({
+  subsets: ['arabic'],
+  weight: ['400', '500', '700'],
+});
+export const readex_pro = Readex_Pro({
+  subsets: ['arabic'],
+  weight: ['400', '500', '700'],
+});
